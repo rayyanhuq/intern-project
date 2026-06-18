360438434a16d0dc14892fc8a20624a159de1170
Rayyan Huq <rayyanhuqprof@gmail.com>
2026-06-18 19:05:13 +0530

    Fix CORS for deployment

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://intern-project-ten-mu.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
